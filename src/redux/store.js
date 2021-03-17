b6c167391b8ed8703da23873eb00866bb5287a4b
Gideon Chrapko <gideonchrapko@hotmail.ca>
2021-03-16 18:55:03 -0700

    re attempt

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,7 @@
   function saveToLocalStorage(state) {
     try {
       const serializedState = JSON.stringify(state)
-      localStorage.setItem('state', serializedState.shopifyState.checkout)
+      localStorage.setItem('state', serializedState)
     } catch(e) {
       console.log(e)
     }
